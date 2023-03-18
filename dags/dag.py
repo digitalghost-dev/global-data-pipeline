4a538491fae086e31d52129d61cc4e7481f0c449
digitalghost-dev <pythonsanchez@gmail.com>
2023-03-18 14:33:59 -0700

    building out DAGs

--- dags/dag.py
+++ dags/dag.py
@@ -1,21 +1,40 @@
 from airflow import DAG
-from datetime import datetime
+from datetime import datetime, timedelta
 
-try:
-    from docker.types import Mount
-    from airflow.providers.docker.operators.docker import DockerOperator
-    from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
-    from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-except ImportError:
-    print("Couldn't import docker.types and/or DockerOperator modules.")
+from docker.types import Mount
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.discord.operators.discord_webhook import DiscordWebhookOperator
 
 bucket = 'global-data-storage-bucket'
-table = 'cloud-data-infrastructure.global_data_dataset.city_weather'
+table = 'cloud-data-infrastructure.global_data_dataset.'
 
-# Coordinates Table
-with DAG("city_coordinates", start_date=datetime(2023, 1, 1), schedule_interval='*/10 * * * *', catchup=False) as dag:
+default_args = {
+    'owner': 'Airflow',
+    'start_date': datetime(2023, 1, 1),
+    'email': ['christian@digitalghost.dev'],
+    'email_on_failure': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
-    task_1 = DockerOperator (
+# Discord notification
+with DAG("discord_alert", default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
+
+    discord_task = DiscordWebhookOperator (
+        task_id="discord_message",
+        http_conn_id="discord",
+        message="Hello from Airflow!"
+    )
+
+discord_task
+
+# coordinates table
+with DAG("city_coordinates", default_args=default_args, schedule_interval='*/20 * * * *', catchup=False) as dag:
+
+    task_a = DockerOperator (
         task_id="1-run_docker_container",
         image='digitalghostdev/global-data-pipeline:city_coordinates',
         command='python3 city_coordinates.py',
@@ -30,8 +49,39 @@ with DAG("city_coordinates", start_date=datetime(2023, 1, 1), schedule_interval=
         ]
     )
 
-# Country Statistics Table
-with DAG("country_statistics", start_date=datetime(2023, 1, 1), schedule_interval='@hourly', catchup=False) as dag:
+    task_b = PostgresToGCSOperator (
+        task_id="postgres_to_cloud_storage",
+        postgres_conn_id='postgres_default',
+        sql='SELECT * FROM "gd.city_coordinates";',
+        bucket=bucket,
+        filename='city_coordinates.csv',
+        export_format='csv',
+        gzip=False,
+        use_server_side_cursor=False,
+    )
+
+    task_c = GCSToBigQueryOperator (
+        task_id="cloud_storage_to_bigquery",
+        gcp_conn_id='google_cloud_default',
+        bucket=bucket,
+        source_objects=['city_coordinates.csv'],
+        source_format='CSV',
+        destination_project_dataset_table=(table + "city_coordinates"),
+        schema_fields = [
+            {'name': 'city', 'type': 'STRING'},
+            {'name': 'lat', 'type': 'FLOAT'},
+            {'name': 'lon', 'type': 'FLOAT'},
+            {'name': 'coordinates', 'type': 'STRING'},
+        ],
+        create_disposition='CREATE_IF_NEEDED',
+        write_disposition='WRITE_TRUNCATE',
+        skip_leading_rows=1,
+    )
+
+task_a >> task_b >> task_c
+
+# country statistics table
+with DAG("country_statistics", default_args=default_args, schedule_interval='@monthly', catchup=False) as dag:
 
     task_a = DockerOperator (
         task_id="task_a",
@@ -48,8 +98,160 @@ with DAG("country_statistics", start_date=datetime(2023, 1, 1), schedule_interva
         ]
     )
 
-# Weather Table
-with DAG("city_weather", start_date=datetime(2023, 1, 1), schedule_interval='@hourly', catchup=False) as dag:
+    # Fertility table tasks.
+    task_b = PostgresToGCSOperator (
+        task_id="postgres_to_cloud_storage_fertility",
+        postgres_conn_id='postgres_default',
+        sql='SELECT * FROM "gd.fertility";',
+        bucket=bucket,
+        filename='fertility.csv',
+        export_format='csv',
+        gzip=False,
+        use_server_side_cursor=False,
+    )
+
+    task_c = GCSToBigQueryOperator (
+        task_id="cloud_storage_to_bigquery_fertility",
+        gcp_conn_id='google_cloud_default',
+        bucket=bucket,
+        source_objects=['fertility.csv'],
+        source_format='CSV',
+        destination_project_dataset_table=(table + "fertility"),
+        schema_fields = [
+            {'name': 'country', 'type': 'STRING'},
+            {'name': 'fertility_Rate', 'type': 'FLOAT'},
+        ],
+        create_disposition='CREATE_IF_NEEDED',
+        write_disposition='WRITE_TRUNCATE',
+        skip_leading_rows=1,
+    )
+
+    # Homicide table tasks.
+    task_d = PostgresToGCSOperator (
+        task_id="postgres_to_cloud_storage_homicide",
+        postgres_conn_id='postgres_default',
+        sql='SELECT * FROM "gd.homicide";',
+        bucket=bucket,
+        filename='homicide.csv',
+        export_format='csv',
+        gzip=False,
+        use_server_side_cursor=False,
+    )
+
+    task_e = GCSToBigQueryOperator (
+        task_id="cloud_storage_to_bigquery_homicide",
+        gcp_conn_id='google_cloud_default',
+        bucket=bucket,
+        source_objects=['homicide.csv'],
+        source_format='CSV',
+        destination_project_dataset_table=(table + "homicide"),
+        schema_fields = [
+            {'name': 'country', 'type': 'STRING'},
+            {'name': 'region', 'type': 'STRING'},
+            {'name': 'subregion', 'type': 'STRING'},
+            {'name': 'rate', 'type': 'FLOAT'},
+            {'name': 'count', 'type': 'INTEGER'},
+            {'name': 'last_updated', 'type': 'INTEGER'},
+        ],
+        create_disposition='CREATE_IF_NEEDED',
+        write_disposition='WRITE_TRUNCATE',
+        skip_leading_rows=1,
+    )
+
+    # Obesity table tasks.
+    task_f = PostgresToGCSOperator (
+        task_id="postgres_to_cloud_storage_obesity",
+        postgres_conn_id='postgres_default',
+        sql='SELECT * FROM "gd.obesity";',
+        bucket=bucket,
+        filename='obesity.csv',
+        export_format='csv',
+        gzip=False,
+        use_server_side_cursor=False,
+    )
+
+    task_g = GCSToBigQueryOperator (
+        task_id="cloud_storage_to_bigquery_obesity",
+        gcp_conn_id='google_cloud_default',
+        bucket=bucket,
+        source_objects=['obesity.csv'],
+        source_format='CSV',
+        destination_project_dataset_table=(table + "obesity"),
+        schema_fields = [
+            {'name': 'country', 'type': 'STRING'},
+            {'name': 'obesity_rate_percentage', 'type': 'FLOAT'},
+        ],
+        create_disposition='CREATE_IF_NEEDED',
+        write_disposition='WRITE_TRUNCATE',
+        skip_leading_rows=1,
+    )
+
+    # Unemployment table tasks.
+    task_h = PostgresToGCSOperator (
+        task_id="postgres_to_cloud_storage_unemployment",
+        postgres_conn_id='postgres_default',
+        sql='SELECT * FROM "gd.unemployment";',
+        bucket=bucket,
+        filename='unemployment.csv',
+        export_format='csv',
+        gzip=False,
+        use_server_side_cursor=False,
+    )
+
+    task_i = GCSToBigQueryOperator (
+        task_id="cloud_storage_to_bigquery_unemployment",
+        gcp_conn_id='google_cloud_default',
+        bucket=bucket,
+        source_objects=['hdi.csv'],
+        source_format='CSV',
+        destination_project_dataset_table=(table + "hdi"),
+        schema_fields = [
+            {'name': 'country', 'type': 'STRING'},
+            {'name': 'unemployment_rate', 'type': 'FLOAT'},
+            {'name': 'last_updated', 'type': 'STRING'},
+        ],
+        create_disposition='CREATE_IF_NEEDED',
+        write_disposition='WRITE_TRUNCATE',
+        skip_leading_rows=1,
+    )
+
+    # HDI table tasks.
+    task_j = PostgresToGCSOperator (
+        task_id="postgres_to_cloud_storage_hdi",
+        postgres_conn_id='postgres_default',
+        sql='SELECT * FROM "gd.hdi";',
+        bucket=bucket,
+        filename='hdi.csv',
+        export_format='csv',
+        gzip=False,
+        use_server_side_cursor=False,
+    )
+
+    task_k = GCSToBigQueryOperator (
+        task_id="cloud_storage_to_bigquery_hdi",
+        gcp_conn_id='google_cloud_default',
+        bucket=bucket,
+        source_objects=['hdi.csv'],
+        source_format='CSV',
+        destination_project_dataset_table=(table + "hdi"),
+        schema_fields = [
+            {'name': 'country', 'type': 'STRING'},
+            {'name': 'hdi', 'type': 'FLOAT'},
+            {'name': 'hdi_growth', 'type': 'FLOAT'},
+        ],
+        create_disposition='CREATE_IF_NEEDED',
+        write_disposition='WRITE_TRUNCATE',
+        skip_leading_rows=1,
+    )
+
+task_a >> task_b >> task_c
+task_a >> task_d >> task_e
+task_a >> task_f >> task_g
+task_a >> task_h >> task_i
+task_a >> task_j >> task_k
+
+# weather table
+with DAG("city_weather", default_args=default_args, schedule_interval='@hourly', catchup=False) as dag:
 
     task_a = DockerOperator (
         task_id="run_docker_container",
@@ -83,7 +285,7 @@ with DAG("city_weather", start_date=datetime(2023, 1, 1), schedule_interval='@ho
         bucket=bucket,
         source_objects=['city_weather.csv'],
         source_format='CSV',
-        destination_project_dataset_table=table,
+        destination_project_dataset_table=(table + "city_weather"),
         schema_fields = [
             {'name': 'city', 'type': 'STRING'},
             {'name': 'current_temp', 'type': 'INTEGER'},
@@ -98,8 +300,8 @@ with DAG("city_weather", start_date=datetime(2023, 1, 1), schedule_interval='@ho
 
 task_a >> task_b >> task_c
 
-# Population Table
-with DAG("population", start_date=datetime(2023, 1, 1), schedule_interval='@hourly', catchup=False) as dag:
+# population table
+with DAG("population", default_args=default_args, schedule_interval='@hourly', catchup=False) as dag:
 
     task_a = DockerOperator (
         task_id="task_a",
@@ -115,3 +317,34 @@ with DAG("population", start_date=datetime(2023, 1, 1), schedule_interval='@hour
             )
         ]
     )
+
+    task_b = PostgresToGCSOperator (
+        task_id="postgres_to_cloud_storage",
+        postgres_conn_id='postgres_default',
+        sql='SELECT * FROM "gd.city_pop";',
+        bucket=bucket,
+        filename='city_pop.csv',
+        export_format='csv',
+        gzip=False,
+        use_server_side_cursor=False,
+    )
+
+    task_c = GCSToBigQueryOperator (
+        task_id="cloud_storage_to_bigquery",
+        gcp_conn_id='google_cloud_default',
+        bucket=bucket,
+        source_objects=['city_pop.csv'],
+        source_format='CSV',
+        destination_project_dataset_table=(table + "city_pop"),
+        schema_fields = [
+            {'name': 'rank', 'type': 'INTEGER'},
+            {'name': 'city', 'type': 'STRING'},
+            {'name': 'country', 'type': 'STRING'},
+            {'name': 'population', 'type': 'BIGNUMERIC'},
+        ],
+        create_disposition='CREATE_IF_NEEDED',
+        write_disposition='WRITE_TRUNCATE',
+        skip_leading_rows=1,
+    )
+
+task_a >> task_b >> task_c
